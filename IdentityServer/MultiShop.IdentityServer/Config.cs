d9dcc1770b8369fbfeba30e702516a5fe05d5732
aziz.akpur <aziz.akpur@NA06069>
2024-09-18 11:15:21 +0300

    Identity server eklendi. Configleri düzenlendi

---
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace MultiShop.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("ResourceCatalog")//buna sahip olan kullanıcı alttaki işlemi gerçekleştirebilir
+            {
+                Scopes={"CatalogFullPermission","CatalogReadPermission"}
+            }
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[] //catalogfullpermission a sahip kullanıcı ne işlemleri yapabilir
+        {
+            new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
+            new ApiScope("CatalogReadPermission","Reading autority for catalog operations")
+        };
+
+
+
+    }
+}
